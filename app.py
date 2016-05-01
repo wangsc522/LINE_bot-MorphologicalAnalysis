fc1e1f906e3d6945a1835608035491cfa90772af
nnsnodnb <ahr63_gej@me.com>
2016-05-01 16:53:44 +0900

    Update settings

--- app.py
+++ app.py
@@ -5,22 +5,24 @@ import requests
 import json
 import re
 
+import settings
+
 LINEBOT_API_EVENT ='https://trialbot-api.line.me/v1/events'
 LINE_HEADERS = {
     'Content-type': 'application/json; charset=UTF-8',
-    'X-Line-ChannelID':'1466099631', # Channel ID
-    'X-Line-ChannelSecret':'1255afe6c79053d174d67a6d3d310eb5', # Channel secre
-    'X-Line-Trusted-User-With-ACL':'ue791aeb4fd39f39d9a9460e80bd07490' # MID (of Channel)
+    'X-Line-ChannelID':settings.CHANNEL_ID,
+    'X-Line-ChannelSecret':settings.CHANNEL_SECRET,
+    'X-Line-Trusted-User-With-ACL':settings.MID
 }
 
 def post_event(to, content):
     msg = {
         'to': [to],
-        'toChannel': 1383378250, # Fixed  value
-        'eventType': "138311608800106203", # Fixed value
+        'toChannel': 1383378250,
+        'eventType': "138311608800106203",
         'content': content
     }
-    r = requests.post(LINEBOT_API_EVENT, headers=LINE_HEADERS, data=json.dumps(msg))
+    r = requests.post(LINEBOT_API_EVENT, headers = LINE_HEADERS, data = json.dumps(msg))
 
 def post_text(to, text):
     content = {
@@ -32,29 +34,27 @@ def post_text(to, text):
 
 
 commands = (
-    (re.compile('ラッシャー', 0), lambda x: 'テメエコノヤロウ'),
-    (re.compile('ダンカン', 0), lambda x:'バカヤロコノヤロウ'),
+    (re.compile('作者', 0), lambda x: 'https://nnsodnb.moe'),
 )
 
 app = Flask(__name__)
 
 @app.route("/callback", methods=['POST'])
-def hello():
-    msgs = request.json['result']
-    for msg in msgs:
-        text = msg['content']['text']
+def callback():
+    message = request.json['result']
+    print(message)
+    for message in masseges:
+        text = message['content']['text']
         for matcher, action in commands:
             if matcher.search(text):
                 response = action(text)
                 break
         else:
-            response = 'コマネチ'
+            response = 'コマンド以外の言葉'
 
-        post_text(msg['content']['from'],response)
+        post_text(message['content']['from'],response)
 
     return ''
 
 if __name__ == "__main__":
-    #context = ('cert/server.pem', 'cert/privkey.pem')
-    #app.run(host = '0.0.0.0', port = 443, ssl_context = context, threaded = True, debug = True)
     app.run(host = '0.0.0.0', port = 8001, threaded = True, debug = True)
